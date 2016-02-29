2abdaec4b374734bf9a156727193f320a304ba00
CharlesBao <charlesbao0319@gmail.com>
2016-02-29 16:31:59 +0800

    Added files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Section = require('../js/components/section.react');
+
+ReactDOM.render(
+    <Section />,
+    document.getElementById('main')
+);
